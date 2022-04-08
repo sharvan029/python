ec2bab249f8baea2df4ab6cf9990b1adab90668e
sharvan <sharvan029@gmail.com>
2022-04-08 23:09:37 +0530

    Update DefaultDict.py

--- PythonHackerRank/DefaultDict.py
+++ PythonHackerRank/DefaultDict.py
@@ -1,16 +1,8 @@
 from collections import defaultdict
+
+n,m = list(map(int,input().split()))
 d = defaultdict(list)
-n, m = map(int, input().split())
-nlist = []
-mlist = []
 for i in range(n):
-    nlist.append(input())
-for j in range(m):
-    mlist.append(input())
-d['groupa'].append(nlist)
-d['groupb'].append(mlist)
-
-for b in d['groupb']:
-    print(b)
-
-
+    d[input()].append(i+1)
+for i in range(m):
+    print(*d[input()] or [-1])
