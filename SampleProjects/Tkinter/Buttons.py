0ae000144e26a99e0bee0d3ef990eed86a459751
sharvan <sharvan029@gmail.com>
2022-05-01 22:22:01 +0530

    Added Tkinter examples

---
+++ SampleProjects/Tkinter/Buttons.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+m = tk.Tk()
+m.title('Counting Seconds')
+button = tk.Button(m, text='Stop', width=25, command=m.destroy)
+button.pack()
+
+
+def hellocallback():
+    tkm.showinfo("Hello Python", "Hello World")
+
+
+B = tk.Button(m, text="Click here", bg="Red", activebackground="Blue", command= hellocallback)
+
+B.pack()
+m.mainloop()
+
+'''
+widgets are added here
+'''
